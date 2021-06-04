5b15996fc2cfb43f8c7ebfd4d15c8dd822feb81a
xiby <15555438336@163.com>
2021-06-04 19:42:59 +0800

    [implement]recover the model from tensor

--- src/LearningAgent.py
+++ src/LearningAgent.py
@@ -1,4 +1,6 @@
 import torch
+
+from collections import OrderedDict
 class LearningAgent():
     def __init__(self, id, model, loss_fn, optimizor, train_dataloader, test_dataloader):
         super().__init__()
@@ -9,7 +11,7 @@ class LearningAgent():
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
         self.param = model.state_dict()
-    def flattern(self):
+    def flatten(self):
         '''
         flattern the param
         '''
@@ -20,4 +22,17 @@ class LearningAgent():
             else:
                 ret = torch.cat(ret, torch.flatten(self.param[key]))
         return ret
+    
+    def recoverModel(self, T):
+        '''
+        translate the flattened param to model
+        '''
+        index = 0
+        trueParam = OrderedDict()
+        for key in self.keys():
+            m,n,h,w = self.param[key].shape
+            target_index = index + m*n*h*w
+            sub_tensor = T[index:target_index]
+            trueParam[key] = torch.reshape(sub_tensor, (m,n,h,w))
+            index = target_index
 
